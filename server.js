9dfec4d4d5f61a7b88964ca7ec7bbd6a6a52a53a
Cyrille Labesse <cyrille@inomads.hk>
2017-09-17 12:11:36 -0700

    In construction

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.all('*', function(req, res, next) {
 
 app.set('port', process.env.PORT || 3000);
 
+app.get('/', function(req, res) {
+  res.end('In construction!');
+});
+
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
